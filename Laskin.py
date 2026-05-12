4850d7b49e1a63d0eb0764d1c9d883ea1f66d17a
TuliTheGolf <juho.pietarinen@edu.keuda.fi>
2026-05-12 13:19:07 +0300

    Lisätty jakolasku-funktio

--- Laskin.py
+++ Laskin.py
@@ -22,5 +22,15 @@ print("5 + 3 =", yhteenlasku(5, 3))
 print("10 - 4 =", vähennyslasku(10, 4))
 print("10 * 5 =", kertolasku(10, 5))
 
+def jakolasku(a, b):
+    return a - b
+ 
+print("Laskin v1.0")
+print("5 + 3 =", yhteenlasku(5, 3))
+print("10 - 4 =", vähennyslasku(10, 4))
+print("10 * 5 =", kertolasku(10, 5))
+print("10 / 2 =", jakolasku(10, 2))
+
+
 
 
