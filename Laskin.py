c02d46b29afbee02f609ed61e258d3a553defcba
juho.pietarinen <juho.pietarinen@edu.keuda.fi>
2026-05-12 11:51:36 +0300

    Lisätty yhteenlasku-funktio

--- Laskin.py
+++ Laskin.py
@@ -1,5 +1,18 @@
-# Laskin v1.0
-print("Laskin v1")
+def yhteenlasku(a, b):
+    return a + b
+ 
+print("Laskin v1.0")
+print("5 + 3 =", yhteenlasku(5, 3))
+
+def yhteenlasku(a, b):
+    return a + b
+ 
+def vähennyslasku(a, b):
+    return a - b
+ 
+print("Laskin v1.0")
+print("5 + 3 =", yhteenlasku(5, 3))
+print("10 - 4 =", vähennyslasku(10, 4))
 
 
 
